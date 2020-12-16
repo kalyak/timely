988525d9586439e2a916cbb052dee435f660c5a1
kkusumad <kalyakusumadi@gmail.com>
2020-12-16 21:36:45 +0800

    date time conversion successful.

--- timely/src/components/ConvertTime.js
+++ timely/src/components/ConvertTime.js
@@ -4,13 +4,17 @@ import utc from "dayjs/plugin/utc"; // dependent on utc plugin
 import timezone from "dayjs/plugin/timezone";
 import Groups from "./Groups_conversion";
 import TimeNow from "./TimeNow";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+// Import the library
+import Datetime from "react-datetime";
+import "react-datetime/css/react-datetime.css";
+
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
 const What = ({ user, contacts, setContacts, currentTime }) => {
   //   let now = dayjs.tz("2020-12-16 15:04", "America/New_York");
-  const time = "2020-12-16 15:04";
+  const [time, timeToConvert] = useState(dayjs().format("YYYY-MM-DD HH:mm"));
 
   const ConvertTime = (timezone) => {
     console.log("retrieving", timezone);
@@ -36,11 +40,49 @@ const What = ({ user, contacts, setContacts, currentTime }) => {
       }
       setContacts(contactsNew);
     }
-  }, [contacts.length]);
+  }, [contacts.length, time]);
+
+  // const convertTime = (timePicked) => {
+  //   console.log("converting");
+  //   timeToConvert(timePicked);
+  //   console.log(time);
+
+  //   if (contacts.length !== 0) {
+  //     console.log("mapping");
+  //     const convertTimeArray = contacts.map((contact, i) =>
+  //       ConvertTime(contact.timezone)
+  //     );
+  //     const contactsNew = [...contacts];
+  //     for (let i = 0; i < contacts.length; i++) {
+  //       const contactToChange = { ...contactsNew[i] };
+  //       contactToChange.gmt = convertTimeArray[i].format("Z");
+  //       contactToChange.convertedDate = convertTimeArray[i].format(
+  //         "ddd, DD-MMM-YYYY"
+  //       );
+  //       contactToChange.convertedTime = convertTimeArray[i].format("hh:mm A");
+  //       contactsNew[i] = contactToChange;
+  //       // console.log(convertTimeArray[i].toString());
+  //     }
+  //     setContacts(contactsNew);
+  //   }
+  //   console.log("end time change");
+  // };
 
   return (
     <div>
       <h2>DAYJS</h2>
+
+      <form>
+        <label htmlFor='date-picker'>Convert from: </label>
+        <Datetime
+          id='date-picker'
+          initialValue={time}
+          onChange={(response) => {
+            timeToConvert(response.format("YYYY-MM-DD HH:mm"));
+          }}
+        />
+      </form>
+
       <Groups contacts={contacts} setContacts={setContacts} />
     </div>
   );
--- timely/src/components/TimeNow.js
+++ timely/src/components/TimeNow.js
@@ -6,14 +6,16 @@ const TimeNow = ({ user, currentTime, setCurrentTime }) => {
     const timer = setTimeout(() => {
       const date = dayjs().format("ddd, DD MMM YYYY");
       const time = dayjs().format("hh:mm:ss A");
-      setCurrentTime({ date: date, time: time });
+      const timezone = dayjs().format("Z");
+      setCurrentTime({ date: date, time: time, timezone: timezone });
     }, 1000);
     return () => clearTimeout(timer);
   });
 
   return (
     <div>
-      Welcome {user}, it is now {currentTime.time} on {currentTime.date}
+      Welcome {user}, it is now {currentTime.time} on {currentTime.date}. You
+      are in timezone GMT {currentTime.timezone}.
       {/* Welcome {user}, it is now {currentTime} */}
     </div>
   );
